063450c6b98da62636d1810aee07f96b903c3fed
Pedro Almir <petrus.cc@gmail.com>
2014-08-12 08:55:56 -0300

    Initial commit

---
+++ src/com/taskadapter/redmineapi/bean/Tracker.java
@@ -0,0 +1,58 @@
+package com.taskadapter.redmineapi.bean;
+
+import java.io.Serializable;
+
+/**
+ * Redmine's Tracker (bug/feature/task/...)
+ */
+public class Tracker implements Identifiable, Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 4748609310534179493L;
+	private Integer id;
+    private String name;
+
+    public Tracker() {
+    }
+
+    public Tracker(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tracker tracker = (Tracker) o;
+
+        if (id != null ? !id.equals(tracker.id) : tracker.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
